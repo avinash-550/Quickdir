d07605309f4bb8df6d920e6857b76522db2ba506
Avinash <59113057+Viwarrior@users.noreply.github.com>
2020-11-22 20:56:59 +0530

    initial commit

---
+++ Quickdir/config.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 11:38:35 2020
+
+@author: AVINASH
+"""
+
+json = {"subname": {
+                "cat1": {
+                        "alternate material": 0,
+                        "notes.txt": 1
+                    },
+                "cat2": {
+                        "alternate material": 0,
+                        "notes.txt": 1
+                    },
+                "lab" : {
+                        "1":0,
+                        "2":0, 
+                        "3":0,
+                        "4":0,
+                        "5":0,
+                        "midterm":0,
+                        "labfat":0,
+                        "notes.txt": 1
+                    },
+                "DA": {
+                        "1":0,
+                        "2":0
+                    },
+                "FAT":{
+                        "notes.txt" :1
+                    },
+                "project": {
+                        "review 1": 0,
+                        "review 2": 0,
+                        "review 3": 0,
+                        "code": {
+                                  "test.py":1  
+                        }
+                    },
+                
+                "syllabus": 0
+                
+    } 
+    }
+
---
+++ Quickdir/quick dir.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 08:58:59 2020
+
+@author: AVINASH
+"""
+
+import os
+import config
+
+class Quickdir:
+    def __init__(self,path="jdirec"):
+        """ initialize path or set it to empty if no argument given"""
+        self.path = path
+        try:
+            os.mkdir(self.path + '/')
+        except:
+            pass
+    
+    def make(self, json, path = ""):
+        """recursively traverse json object and make directories."""
+        
+        for key, value in json.items():
+            pathr = path
+            path += key + '/'
+            if value == 1:
+                path = path[:-1]
+                f = open(self.path + '/' + path,'a')
+                f.close()
+                path = pathr
+                continue
+            elif value == 0:
+                os.mkdir(self.path + '/' + path)
+                path = pathr
+                continue
+            else:
+                os.mkdir(self.path + '/' + path)
+                self.make(value, path)
+                path = pathr
+                
+            
+
+
+if __name__ == "__main__":
+    
+    quick = Quickdir("hello")
+    quick.make(config.json)
+      
+        
