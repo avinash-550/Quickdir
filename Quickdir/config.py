3560bb81449614a0256904765c100aad1beb01bc
Avinash <59113057+Viwarrior@users.noreply.github.com>
2020-11-22 21:42:47 +0530

    Update config.py

--- Quickdir/config.py
+++ Quickdir/config.py
@@ -5,43 +5,31 @@ Created on Sun Nov 22 11:38:35 2020
 @author: AVINASH
 """
 
-json = {"subname": {
-                "cat1": {
-                        "alternate material": 0,
-                        "notes.txt": 1
+json = {"root": {
+                "logs": {
+                        "test": 0,
+                        "log.txt": 1
                     },
-                "cat2": {
-                        "alternate material": 0,
-                        "notes.txt": 1
+                "css" : {
+                        "style.css" : 1,
+                        "bootstrap.css": 1
                     },
-                "lab" : {
-                        "1":0,
-                        "2":0, 
-                        "3":0,
-                        "4":0,
-                        "5":0,
-                        "midterm":0,
-                        "labfat":0,
-                        "notes.txt": 1
+                "js": {
+                        "mainFunction.js":1,
+                        "bootstrap.js":1,
+                        "jquery.js":1
                     },
-                "DA": {
-                        "1":0,
-                        "2":0
+                "common":{
+                        "navbar.html" :1,
+                        "footer.html": 1
                     },
-                "FAT":{
-                        "notes.txt" :1
-                    },
-                "project": {
-                        "review 1": 0,
-                        "review 2": 0,
-                        "review 3": 0,
-                        "code": {
-                                  "test.py":1  
-                        }
-                    },
+                "Readme.md": 1,
+                "fonts": {
+                    "sample.txt":1
+                  },
+                "img": 0,
+                "index.html":1
                 
-                "syllabus": 0
-                
-    } 
+        } 
     }
 
